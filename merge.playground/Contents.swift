0a43438461e7c0572206567df7ff019497100978
Corey Davis <cdavis@captechconsulting.com>
2017-11-29 10:14:13 -0700

    [FEAT] Create a rando number game that can be played and won
    * [FEAT] Add rando number func
    the rando() func returns an INT between 0-10
    
    * [WIP] Working on building the game now
    The new func check the value of the rando func and tells us if there
    is a winner
    
    * [FEAT] The game is complete
    Finish up the game with the lets play func, so lets merge this to
    master now using github!
    
    
    I may also want to include details about the ticket items, links to JIRA / Conf, or screen shots here

--- merge.playground/Contents.swift
+++ merge.playground/Contents.swift
@@ -4,3 +4,35 @@ import UIKit
 
 var str = "Hello, playground"
 
+func rando() -> Int {
+    let num = Int(arc4random_uniform(10))
+    return num
+}
+
+func checkWinner() -> Bool {
+    let roll = rando()
+
+    if roll > 2 {
+        return true
+    } else if roll > 8 {
+        return false
+    }
+
+    return false
+}
+
+func letsPlay() {
+    if checkWinner() {
+        print("YOU WIN!!!!!!!!")
+    } else {
+        print("You stink")
+    }
+}
+
+letsPlay()
+
+letsPlay()
+
+letsPlay()
+
+letsPlay()
